50c816ad1dad8808d9167a3fb40f828f2a09e245
Maksym Lepekh <mlepekh-ua@singlestore.com>
2025-04-11 20:52:07 +0300

    Modify project to use Clang 20 and alpine linux

--- no_module.cpp
+++ no_module.cpp
@@ -1,5 +1,6 @@
 #include <format>
 #include <string>
+#include <type_traits>
 
 class refactor_me
 {
@@ -18,4 +19,15 @@ auto bar() -> std::string
     using namespace std::literals;
     auto r = refactor_me{"abc"sv};
     return std::format("Hello {}", r.get_text());
+}
+
+template <typename T>
+concept Foo = requires(T t)
+{
+    { t.method1() } -> std::same_as<std::string>;
+};
+
+
+auto test_foo(Foo auto arg1) -> std::string {
+    return arg1.method1();
 }
